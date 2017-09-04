00c95102092ba12cde0e51d13570391dbea83a1c
Gerdje-hc <gerdje.hc@gmail.com>
2017-09-04 07:13:21 +0200

    Update from homework

---
+++ oefeningPG1.js
@@ -0,0 +1,9 @@
+
+//alert(document.getElementById("demo").innerHTML = "Paragraph changed");
+
+alert(document.getElementById("demo").innerHTML);  // door de inner HTML gaat de output in de code van de paragraaf kijken
+
+    document.write("Name" + "<input type=\"text\" name=\"fName\"></input>");
+
+
+
